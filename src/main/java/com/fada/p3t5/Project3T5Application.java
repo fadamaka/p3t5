275481e19032e1e39a4f5423fa1ec3d182f8749e
fadamaka <tamas.hercz@cubicfox.com>
2021-11-08 10:35:33 +0100

    initialized spring reactive project

---
+++ src/main/java/com/fada/p3t5/Project3T5Application.java
@@ -0,0 +1,13 @@
+package com.fada.p3t5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3T5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3T5Application.class, args);
+	}
+
+}
